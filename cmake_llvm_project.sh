a138b7617e82effcd62f10785ac68f961578b01f
Nikita Popov <nikita.ppv@gmail.com>
2020-03-11 21:52:33 +0000

    Fix typo

--- cmake_llvm_project.sh
+++ cmake_llvm_project.sh
@@ -2,5 +2,5 @@ cmake -GNinja -H./llvm-project/llvm -B./llvm-project-build \
     -DLLVM_ENABLE_PROJECTS="clang;compiler-rt" \
     -DLLVM_TARGETS_TO_BUILD="X86" \
     -DCMAKE_BUILD_TYPE=Release \
-    -DLLVM_CMAKE_BUILD=true \
+    -DLLVM_CCACHE_BUILD=true \
     -DLLVM_USE_LINKER=gold
