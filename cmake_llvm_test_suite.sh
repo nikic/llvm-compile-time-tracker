4a7fd92add4b23ccd85150ef22312a0f6fa82886
Nikita Popov <nikita.ppv@gmail.com>
2020-03-12 17:53:13 +0000

    Updates

---
+++ cmake_llvm_test_suite.sh
@@ -0,0 +1,6 @@
+cmake -GNinja -H./llvm-test-suite -B./llvm-test-suite-build \
+    -C./llvm-test-suite/cmake/caches/O3.cmake \
+    -DCMAKE_C_COMPILER=$PWD/llvm-project-build/bin/clang \
+    -DTEST_SUITE_USE_PERF=true \
+    -DTEST_SUITE_SUBDIRS=CTMark \
+    -DTEST_SUITE_RUN_BENCHMARKS=false
