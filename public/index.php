30056d3e0c2666c4658b45a83363772f3b657a6f
Nikita Popov <nikita.ppv@gmail.com>
2020-03-14 15:28:35 +0100

    Support multiple branches

--- public/index.php
+++ public/index.php
@@ -2,50 +2,61 @@
 
 $dataDir = __DIR__ . '/../data';
 $commitsFile = $dataDir . '/commits.json';
-$stat = "instructions";
 
-$commitsData = json_decode(file_get_contents($commitsFile), true);
-$titles = null;
-$rows = [];
-$lastMetrics = null;
-foreach ($commitsData['origin'] as $commit) {
-    $hash = $commit['hash'];
-    $summary = getSummary($dataDir, $hash);
-    $row = [formatHash($hash)];
-    if ($summary) {
-        if (!$titles) {
-            $titles = array_merge(['Commit'], array_keys($summary));
-        }
-        $metrics = array_column($summary, 'instructions');
-        foreach ($metrics as $i => $value) {
-            $prevValue = $lastMetrics[$i] ?? null;
-            if ($prevValue !== null) {
-                $perc = ($value / $prevValue - 1.0) * 100;
-                $row[] = formatMetric($value, $stat) . ' (' . formatPerc($perc) . ')';
-            } else {
-                $row[] = formatMetric($value, $stat);
+$stat = $_GET['stat'] ?? 'instructions';
+
+echo <<<'STYLE'
+<style>
+* { font-family: monospace; }
+table { border-spacing: 1em .1em; }
+</style>
+STYLE;
+echo "\n";
+
+$branchCommits = json_decode(file_get_contents($commitsFile), true);
+foreach ($branchCommits as $branch => $commits) {
+    $titles = null;
+    $rows = [];
+    $lastMetrics = null;
+    foreach ($commits as $commit) {
+        $hash = $commit['hash'];
+        $summary = getSummary($dataDir, $hash);
+        $row = [formatHash($hash)];
+        if ($summary) {
+            if (!$titles) {
+                $titles = array_merge(['Commit'], array_keys($summary));
             }
+            $metrics = array_column($summary, $stat);
+            foreach ($metrics as $i => $value) {
+                $prevValue = $lastMetrics[$i] ?? null;
+                if ($prevValue !== null) {
+                    $perc = ($value / $prevValue - 1.0) * 100;
+                    $row[] = formatMetric($value, $stat) . ' (' . formatPerc($perc) . ')';
+                } else {
+                    $row[] = formatMetric($value, $stat);
+                }
+            }
+            $lastMetrics = $metrics;
         }
-        $lastMetrics = $metrics;
+        $rows[$hash] = $row;
     }
-    $rows[$hash] = $row;
-}
 
-echo "<style>* { font-family: monospace; }</style>\n";
-echo "<table>\n";
-echo "<tr>\n";
-foreach ($titles as $title) {
-    echo "<th>$title</th>\n";
-}
-echo "</tr>\n";
-foreach (array_reverse($rows) as $hash => $row) {
+    echo "<h4>", h($branch), ":</h4>\n";
+    echo "<table>\n";
     echo "<tr>\n";
-    foreach ($row as $value) {
-        echo "<td>$value</td>\n";
+    foreach ($titles as $title) {
+        echo "<th>$title</th>\n";
     }
     echo "</tr>\n";
+    foreach (array_reverse($rows) as $hash => $row) {
+        echo "<tr>\n";
+        foreach ($row as $value) {
+            echo "<td>$value</td>\n";
+        }
+        echo "</tr>\n";
+    }
+    echo "</table>\n";
 }
-echo "</table>\n";
 
 function formatHash(string $hash): string {
     $shortHash = substr($hash, 0, 10);
@@ -53,15 +64,30 @@ function formatHash(string $hash): string {
 }
 
 function formatPerc(float $value): string {
-    return sprintf('%+.2f', $value);
+    $threshold = 0.5;
+    $str = sprintf('%+.2f%%', $value);
+    if ($value > $threshold) {
+        return "<span style=\"color: red\">$str</span>";
+    } else if ($value < -$threshold) {
+        return "<span style=\"color: green\">$str</span>";
+    } else {
+        return $str;
+    }
 }
 
 function formatMetric(float $value, string $metric): string {
-    if ($metric === 'instructions') {
+    switch ($metric) {
+    case 'instructions':
         $m = $value / (1000 * 1000);
         return round($m) . 'M';
+    case 'task-clock':
+        return round($value) . 'ms';
+    case 'max-rss':
+        $k = $value / 1024;
+        return round($k) . 'KiB';
+    default:
+        return (string) $value;
     }
-    return (string) $value;
 }
 
 function h(string $str): string {
