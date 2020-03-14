cd91b43b715bd04634d61e2290a622df5dd019ba
Nikita Popov <nikita.ppv@gmail.com>
2020-03-14 09:48:44 +0000

    Parse commit log

--- runner.php
+++ runner.php
@@ -1,5 +1,6 @@
 <?php
 
+use GitWrapper\GitWorkingCopy;
 use GitWrapper\GitWrapper;
 use Symfony\Component\Process\Process;
 
@@ -7,11 +8,17 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 // Time to sleep if there were no new commits
 $sleepInterval = 5 * 60;
+$firstCommit = '92f7e8133ae98e1f300bad164c4099b2e609bae7';
+$commitsFile = __DIR__ . '/data/commits.json';
 
 $gitWrapper = new GitWrapper();
 $repo = $gitWrapper->workingCopy(__DIR__ . '/llvm-project');
-$prevHead = null;
+$commitsData = [
+    'origin' => getParsedLog($repo, $firstCommit),
+];
+file_put_contents($commitsFile, json_encode($commitsData, JSON_PRETTY_PRINT));
 
+$prevHead = null;
 while (true) {
     $repo->fetch('origin');
     $repo->reset('--hard', 'origin/master');
@@ -33,10 +40,29 @@ while (true) {
 
 function runCommand(string $command) {
     $process = Process::fromShellCommandline($command);
+    $process->setTimeout(null);
     $exitCode = $process->run(function($type, $buffer) {
         echo $buffer;
     });
     if ($exitCode !== 0) {
         throw new Exception("Execution of \"$command\" failed");
     }
+}
+
+function getParsedLog(GitWorkingCopy $repo, string $baseCommit) {
+    $log = $repo->log('--pretty=format:%H;%an;%ae;%cI;%s', '--reverse', "$baseCommit^..");
+    $lines = explode("\n", $log);
+
+    $parsedLog = [];
+    foreach ($lines as $line) {
+        $parts = explode(';', $line, 5);
+        $parsedLog[] = [
+            "hash" => $parts[0],
+            "author_name" => $parts[1],
+            "author_email" => $parts[2],
+            "commit_date" => $parts[3],
+            "subject" => $parts[4]
+        ];
+    }
+    return $parsedLog;
 }
