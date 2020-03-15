acece248eb4bc86e52aaaa40e48e1e4ba2aed649
Nikita Popov <nikita.ppv@gmail.com>
2020-03-15 15:46:30 +0100

    Add geomean in compare view

--- public/compare.php
+++ public/compare.php
@@ -20,48 +20,79 @@ if (!is_string($from) || !is_string($to)) {
     return;
 }
 
-foreach (CONFIGS as $config) {
-    $fromStats = getStats($from, $config);
-    $toStats = getStats($to, $config);
-    if (!$fromStats || !$toStats) {
-        continue;
-    }
+if (!$details) {
+    foreach (CONFIGS as $config) {
+        $fromStats = getSummary($from, $config);
+        $toStats = getSummary($to, $config);
+        if (!$fromStats || !$toStats) {
+            continue;
+        }
+        $fromStats = addGeoMean(array_column_with_keys($fromStats, $stat));
+        $toStats = addGeoMean(array_column_with_keys($toStats, $stat));
 
-    echo "<h4>$config:</h4>\n";
-    echo "<table>\n";
-    echo "<tr>\n";
-    echo "<th>Benchmark</th>";
-    echo "<th>Old</th>";
-    echo "<th>New</th>";
-    echo "<tr>\n";
-    echo "</tr>\n";
-    foreach ($fromStats as $bench => $fromFiles) {
-        $toFiles = $toStats[$bench];
-        $fromAggMetric = aggregateData($fromFiles)[$stat];
-        $toAggMetric = aggregateData($toFiles)[$stat];
+        echo "<h4>$config:</h4>\n";
+        echo "<table>\n";
         echo "<tr>\n";
-        echo "<td style=\"text-align: left\">$bench</td>\n";
-        echo "<td>", formatMetric($fromAggMetric, $stat), "</td>\n";
-        echo "<td>", formatMetricDiff($toAggMetric, $fromAggMetric, $stat), "</td>\n";
+        echo "<th>Benchmark</th>";
+        echo "<th>Old</th>";
+        echo "<th>New</th>";
+        echo "<tr>\n";
         echo "</tr>\n";
-        if ($details) {
-            foreach ($fromFiles as $i => $fromFile) {
-                $toFile = $toFiles[$i];
-                $file = $fromFile['file'];
-                if ($file != $toFile['file']) {
-                    throw new Exception('Mismatch');
+        foreach ($fromStats as $bench => $fromMetric) {
+            $toMetric = $toStats[$bench];
+            echo "<tr>\n";
+            echo "<td style=\"text-align: left\">$bench</td>\n";
+            echo "<td>", formatMetric($fromMetric, $stat), "</td>\n";
+            echo "<td>", formatMetricDiff($toMetric, $fromMetric, $stat), "</td>\n";
+            echo "</tr>\n";
+        }
+        echo "</table>\n";
+    }
+} else {
+    // TODO: Doesn't seem to be terrible useful, possibly remove this?
+    foreach (CONFIGS as $config) {
+        $fromStats = getStats($from, $config);
+        $toStats = getStats($to, $config);
+        if (!$fromStats || !$toStats) {
+            continue;
+        }
+
+        echo "<h4>$config:</h4>\n";
+        echo "<table>\n";
+        echo "<tr>\n";
+        echo "<th>Benchmark</th>";
+        echo "<th>Old</th>";
+        echo "<th>New</th>";
+        echo "<tr>\n";
+        echo "</tr>\n";
+        foreach ($fromStats as $bench => $fromFiles) {
+            $toFiles = $toStats[$bench];
+            $fromAggMetric = aggregateData($fromFiles)[$stat];
+            $toAggMetric = aggregateData($toFiles)[$stat];
+            echo "<tr>\n";
+            echo "<td style=\"text-align: left\">$bench</td>\n";
+            echo "<td>", formatMetric($fromAggMetric, $stat), "</td>\n";
+            echo "<td>", formatMetricDiff($toAggMetric, $fromAggMetric, $stat), "</td>\n";
+            echo "</tr>\n";
+            if ($details) {
+                foreach ($fromFiles as $i => $fromFile) {
+                    $toFile = $toFiles[$i];
+                    $file = $fromFile['file'];
+                    if ($file != $toFile['file']) {
+                        throw new Exception('Mismatch');
+                    }
+                    $fromMetric = $fromFile[$stat];
+                    $toMetric = $toFile[$stat];
+                    echo "<tr>\n";
+                    echo "<td style=\"text-align: left\">&nbsp;&nbsp;&nbsp;&nbsp;$file</td>\n";
+                    echo "<td>", formatMetric($fromMetric, $stat), "</td>\n";
+                    echo "<td>", formatMetricDiff($toMetric, $fromMetric, $stat), "</td>\n";
+                    echo "</tr>\n";
                 }
-                $fromMetric = $fromFile[$stat];
-                $toMetric = $toFile[$stat];
-                echo "<tr>\n";
-                echo "<td style=\"text-align: left\">&nbsp;&nbsp;&nbsp;&nbsp;$file</td>\n";
-                echo "<td>", formatMetric($fromMetric, $stat), "</td>\n";
-                echo "<td>", formatMetricDiff($toMetric, $fromMetric, $stat), "</td>\n";
-                echo "</tr>\n";
             }
         }
+        echo "</table>\n";
     }
-    echo "</table>\n";
 }
 
 function printStatSelect(string $stat) {
--- src/common.php
+++ src/common.php
@@ -2,3 +2,7 @@
 
 const DATA_DIR = __DIR__ . '/../data';
 const CONFIGS = ['O3', 'ReleaseThinLTO', 'ReleaseLTO-g'];
+
+function array_column_with_keys(array $array, $column): array {
+    return array_combine(array_keys($array), array_column($array, $column));
+}
