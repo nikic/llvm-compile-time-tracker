334c5cbb5eb962dfea5c6a1a0ceab3ca3d9215da
Nikita Popov <nikita.ppv@gmail.com>
2020-03-27 09:43:51 +0000

    gzip stats

--- aggregate_data.php
+++ aggregate_data.php
@@ -12,11 +12,11 @@ $inDir = __DIR__ . '/llvm-test-suite-build/CTMark';
 $outDir = __DIR__ . '/data/experiments/' . $name;
 @mkdir($outDir, 0755, true);
 
-$statsFile = $outDir . '/stats.msgpack';
+$statsFile = $outDir . '/stats.msgpack.gz';
 $summaryFile = $outDir . '/summary.json';
 
 $rawData = readRawData($inDir);
 $aggData = array_map('aggregateData', $rawData);
 
-file_put_contents($statsFile, msgpack_pack($rawData));
+file_put_contents($statsFile, gzencode(msgpack_pack($rawData), 9));
 file_put_contents($summaryFile, json_encode($aggData, JSON_PRETTY_PRINT));
