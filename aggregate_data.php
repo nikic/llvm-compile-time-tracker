ddf2ff46889bfe4a2ea54c45044397c193698d21
Nikita Popov <nikita.ppv@gmail.com>
2020-03-27 09:38:11 +0000

    Switch to msgpack

--- aggregate_data.php
+++ aggregate_data.php
@@ -12,11 +12,11 @@ $inDir = __DIR__ . '/llvm-test-suite-build/CTMark';
 $outDir = __DIR__ . '/data/experiments/' . $name;
 @mkdir($outDir, 0755, true);
 
-$statsFile = $outDir . '/stats.json';
+$statsFile = $outDir . '/stats.msgpack';
 $summaryFile = $outDir . '/summary.json';
 
 $rawData = readRawData($inDir);
 $aggData = array_map('aggregateData', $rawData);
 
-file_put_contents($statsFile, json_encode($rawData, JSON_PRETTY_PRINT));
+file_put_contents($statsFile, msgpack_pack($rawData));
 file_put_contents($summaryFile, json_encode($aggData, JSON_PRETTY_PRINT));
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt install -y \
     binutils-dev ccache composer cmake g++ \
     linux-tools-common linux-tools-generic linux-cloud-tools-generic \
-    php-cli php-zip \
+    php-cli php-msgpack php-zip \
     ninja-build tcl
 git clone https://github.com/llvm/llvm-project.git 
 git clone https://github.com/llvm/llvm-test-suite.git 
